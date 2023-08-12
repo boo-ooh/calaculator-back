eb188c811c57bdfc4c849b0108e1d1a917fb3512
Bruna Calabrese <brunacalabrese@gmail.com>
2023-08-12 17:24:39 -0300

    initial commit

---
+++ calculator/src/main/java/com/bruna/calabrese/calculator/controller/OperationController.java
@@ -0,0 +1,9 @@
+package com.bruna.calabrese.calculator.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("operation")
+public class OperationController {
+}
---
+++ calculator/src/test/java/com/bruna/calabrese/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruna.calabrese.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
