0d58d3d1b81e70dbb747c078eb3e0341038302c3
Bruna <brunacalabrese@gmail.com>
2023-08-22 00:13:19 -0300

    add validateToken

--- calculator/src/main/java/com/bruna/calabrese/calculator/controller/AuthenticationController.java
+++ calculator/src/main/java/com/bruna/calabrese/calculator/controller/AuthenticationController.java
@@ -7,11 +7,9 @@ import com.bruna.calabrese.calculator.services.AuthenticationService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth")
@@ -30,6 +28,13 @@ public class AuthenticationController {
         User user = userRepository.findByUsername(dto.username());
 
         return ResponseEntity.ok(new LoginResponseDTO(new UserDTO(user),token));
+    }
+
+    @GetMapping("/validateToken")
+    public ResponseEntity login() {
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        return ResponseEntity.ok(new UserDTO(user));
     }
 
     @PostMapping("/register")
